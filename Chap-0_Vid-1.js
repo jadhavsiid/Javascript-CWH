9ad9318ba7bfd337126f0c6de1caba6f5c199f13
Siddhesh Jadhav <siidjadhav170801@gmail.com>
2022-07-15 14:14:48 +0530

    Day-1

---
+++ Chap-0_Vid-1.js
@@ -0,0 +1,9 @@
+// Printing Hello World in Javascript
+console.log("Hello World.");
+
+// This Program is to add three numbers 
+function addthreenums(a,b,c){
+    return a+b+c;
+}
+let c = addthreenums(1,3,96);
+console.log(c);
