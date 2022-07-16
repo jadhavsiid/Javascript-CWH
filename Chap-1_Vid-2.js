35e0c81c2c0518dc0ef13e305bfccc5b52647937
Siddhesh Jadhav <siidjadhav170801@gmail.com>
2022-07-16 12:47:01 +0530

    Day-2

---
+++ Chap-1_Vid-2.js
@@ -0,0 +1,6 @@
+// Variables in Javascript 
+var a = 67 // a containes 67
+console.log(a)
+a = "Siddhesh"
+console.log(a)
+// here, in line no.2 we have stored a number 67 in 'a' and after at line no.4 we have stored a string "Siddhesh" in 'a', console.log(a) is printing both the values of 'a' as output because JS allows to change type of variable during runtime.
