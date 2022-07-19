c9d459f73c17230c2a772d4ebe03bdf008d14747
Siddhesh Jadhav <siidjadhav170801@gmail.com>
2022-07-19 21:10:27 +0530

    Day-4

---
+++ Chap-1_Vid-4_QQuiz.js
@@ -0,0 +1,22 @@
+// Quick Quiz: WAP a js Program to store name, phone number and marks of a student using object.
+
+const details ={
+    "name":"Siddhesh Jadhav",
+    "Phnumber": 8828911968,
+    "marks": 100
+}
+console.log(details["name"]);
+console.log(details["Phnumber"]);
+console.log(details["marks"]);
+console.log("\n")
+
+// Another example of objects for practise
+const marks ={
+    "Maths": 70,
+    "Science": 55,
+    "English": 62,
+    "Marathi": 65,
+    "Social-Studies": 71
+}
+console.log("These are English's marks:",marks["English"]);
+console.log("These are Math's marks:",marks["Maths"]);
