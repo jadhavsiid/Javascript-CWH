74502cdf0d1d85562680bfc0dc66f2e56a638683
Siddhesh Jadhav <siidjadhav170801@gmail.com>
2022-07-24 13:04:44 +0530

    Day-6

---
+++ Chap-2_Vid-6.js
@@ -0,0 +1,46 @@
+// Vid-6: Javascript Operators and Expressions
+
+console.log ("Operators in js \n");
+console.log ("Arithematic Operators in js");
+let a = 45;
+let b = 4;
+console.log ("a+b =",a+b);
+console.log ("a-b =",a-b);
+console.log ("a*b =",a*b);
+console.log ("a/b =",a/b);
+console.log ("a%b =",a%b);
+console.log ("a**b=",a**b); // Exponential Operator.
+console.log ("a++ =", a++); // first print, then increment.
+console.log ("a-- =", a--); // first print, then decrement.
+console.log ("++a =", ++a); // first increment, then print.
+console.log ("--a =", --a); // firs decrement, then print.
+console.log ("\n");
+
+console.log ("Assignment Operators in js");
+let assignment = 1;
+assignment += 8; // will add 8 to 1
+console.log(assignment,"\n");
+
+console.log ("Comparison Operators in js");
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2: ", comp1 == comp2);
+console.log("comp1 != comp2: ", comp1 != comp2);
+console.log("comp1 === comp2: ", comp1 === comp2);
+console.log("comp1 !== comp2: ", comp1 !== comp2);
+console.log("comp1 > comp2: ", comp1 > comp2);
+console.log("\n");
+
+console.log ("Logical Operators in js");
+let x = 5;
+let y = 8;
+console.log(x>y && x==5);
+console.log(x>y || x==5);
+console.log(x>y || x==5);
+console.log(!false);
+console.log(!true);
+
+// This is a single line comment
+
+/* This 
+is multiline comment */
