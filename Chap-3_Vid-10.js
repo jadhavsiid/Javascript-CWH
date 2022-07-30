9b119e220d8cf4bce62b73320569cbaffb52ee54
Siddhesh Jadhav <siidjadhav170801@gmail.com>
2022-07-30 17:18:13 +0530

    Day-10

---
+++ Chap-3_Vid-10.js
@@ -0,0 +1,15 @@
+// Loops in js 
+// While Loop
+let n = prompt('Enter the value of n')
+n = Number.parseInt(n)
+let i = 0;
+// while (i < n) {
+//     console.log(i)
+//     i++;
+// }
+
+// Do-while Loop
+do {
+    console.log(i)
+    i++;
+} while (i < n)
