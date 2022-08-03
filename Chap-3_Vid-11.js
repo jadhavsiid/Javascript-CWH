f1d50eb2101031e962f472d9808b89ead177ac69
Siddhesh Jadhav <siidjadhav170801@gmail.com>
2022-08-03 21:12:56 +0530

    Add files via upload

---
+++ Chap-3_Vid-11.js
@@ -0,0 +1,22 @@
+// Functions in Js
+function oneplusavg(x,y) {
+  console.log("done")
+  return Math.round (1 + (x + y) / 2);
+}
+let a = 1;
+let b = 2;
+let c = 3;
+console.log(oneplusavg(a,b));
+console.log(oneplusavg(b,c));
+console.log(oneplusavg(a,c));
+
+// Arrow Function [Prefer such syntax for function in js]
+const sum = (a,b)=>{
+    return a+b;
+}
+console.log("The sum is:",sum(8,8));
+// Another example of Arrow Function [Prefer such syntax]
+const hello =()=>{
+    console.error("This is a Cute Error.")
+}
+hello();
---
+++ Chap-3_Vid-12_Prac-Set.js
@@ -0,0 +1,42 @@
+// Chap-3: Practise Set (This Program will run only in replet)
+
+// Q.1) WAP to print marks of a student in an object using for loop
+let marks = {
+  Siddhesh: 99,
+  Lovish: 8,
+  Monica: 4,
+  Pinto: 63,
+};
+for (let i = 0; i < Object.keys(marks).length; i++) {
+  console.log(
+      "The marks of: " +
+      Object.keys(marks)[i] +
+      " are " +
+      marks[Object.keys(marks)[i]]
+  );
+}
+
+// Q.2) WAP to print marks of a student in an object using for-in loop
+console.log("\n");
+for (let key in marks) {
+  console.log("The Marks of " + key + " are " + marks[key])
+}
+
+// Q.3) WAP to print "try again" untill user enters the correct number
+console.log("\n");
+let cn = 4
+let i
+while (i != cn) {
+  i = prompt('Enter a number: ')
+  if (i != cn) {
+      console.log("Try Again.")
+  } else {
+      console.log("You've enterd a correct number.")
+  }
+}
+
+// Q.4) Write a function to remove mean of 5 number
+const mean5 = (a, b, c, d, e) => {
+  return (a + b + c + d + e) / 5;
+}
+console.log("The avg of 3,4,7,9,5 is: ", mean5(3, 4, 7, 9, 5))
