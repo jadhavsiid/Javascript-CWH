8ac4ca957dd9606a42d2c72c4f07e3052419a118
Siddhesh Jadhav <siidjadhav170801@gmail.com>
2022-09-01 22:25:22 +0530

    Add files via upload

---
+++ Chap-6_Vid-27.js
@@ -0,0 +1,14 @@
+// Vid-27: Alert, Prompt and Confirm
+alert("Enter value of a: ");
+let a = prompt("Enter a here: ","578");
+// document.write(a);
+a = Number.parseInt(a);
+alert("You Entered a of type " + typeof a);
+
+let write = confirm('Do you want to write it to the page ?')
+if(write){
+    document.write(a)
+}
+else{
+    document.write('Pleae allow me to write ')
+}
